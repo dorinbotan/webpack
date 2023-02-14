f4204ebe532ce131fe720ac4da856da207ac3105
Dorin Botan <dorinbotan@outlook.com>
2023-02-14 01:30:05 +0000

    acc css, sass, postcss, hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 let mode = 'development';
 
 if (process.env.NODE_ENV === 'production') {
@@ -6,20 +8,34 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
     mode,
-    devtool: 'source-map',
-    devServer: {
-        open: true,
-        static: './dist'
-    },
     module: {
         rules: [
+            {
+                test: /\.(s[ac]|c)css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ]
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                 }
-            }
+            },
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+    ],
+
+    devtool: 'source-map',
+    devServer: {
+        static: './dist',
+        open: true,
+        hot: true,
+    },
 }
